c2793a0897faf24e9c8b23c32b7c0947219534b2
Javier Abadía <javi.abadia@gmail.com>
2020-08-26 09:41:27 +0200

    add special config for one module

--- other_main.py
+++ other_main.py
@@ -6,7 +6,8 @@ from lib1 import useful_module
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    log_utils.config_logging(settings.LOG_LEVEL, settings.LOG_PATH)
+    log_utils.config_logging(logging.WARN, settings.LOG_PATH)
+    logging.getLogger('lib1.useful_module').setLevel(logging.DEBUG)
     logger.debug('this is other main - debug message')
     logger.info('this is other main - info message')
     useful_module.useful_function()
