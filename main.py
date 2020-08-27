2d1ff15cac1afa839f6e4f6cd660a1d45ac21cbe
Javier Abadía <javi.abadia@gmail.com>
2020-08-27 12:57:58 +0200

    remove the need of config_logging() because logging.basicConfig() will suffice

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ from lib1 import useful_module
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    log_utils.config_logging(settings.LOG_LEVEL, log_utils.get_log_path(settings.LOG_FOLDER, __file__))
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname)-8s %(message)s [%(pathname)s:%(lineno)d]',
+        level=settings.LOG_LEVEL,
+        handlers=[  # salida a consola y a fichero
+            logging.StreamHandler(),
+            logging.FileHandler(log_utils.get_log_path(settings.LOG_FOLDER, __file__))
+        ]
+    )
     logger.debug('main debug message')
     logger.info('main info message')
     useful_module.useful_function()
